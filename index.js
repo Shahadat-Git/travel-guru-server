734088c491ef8cb960e121f1eb60f92581454e11
Shahadat-Git <noyon560@gmail.com>
2023-05-22 22:06:30 +0600

    locations data added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const port = 5000;
+const locations = require('./data/locations.json')
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('server is running');
 })
 
-app.listen(port,()=>{
+app.get('/locations', (req, res) => {
+    res.send(locations)
+})
+
+app.listen(port, () => {
     console.log(`server running on this port ${port}`)
 })
