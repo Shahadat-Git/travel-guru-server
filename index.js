f4fc698b59b7992ccd6f3953d689555d6fd062f6
Shahadat-Git <noyon560@gmail.com>
2023-05-22 21:42:51 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+app.listen(port,()=>{
+    console.log(`server running on this port ${port}`)
+})
